d5e466a5a72ee752152b538b60f2d0113f08f4c6
lelchepkoech <lelchepkoech@gmail.com>
2022-03-23 16:59:25 -0700

    create a webpage

---
+++ home.js
@@ -0,0 +1,112 @@
+console.log("hello world ");
+
+console.log("hello world ");
+/*alert("my name is ben");
+var b = "ben";
+console.log(b);
+var num = 10;
+console.log(num);
+prompt("what is your age");
+
+
+var num = 10;
+num++;
+document.write(num);
+num--;
+document.write(num);
+console.log(num % 4);
+console.log(num / 4);
+console.log(num * 4);
+num += 20;
+console.log(num);
+
+function fun() {
+    alert("this is a function");
+}
+fun();
+
+function greetings() {
+    var name = prompt("what is your name");
+    var result = "hello  " + name;
+    console.log(result);
+
+}
+greetings();
+
+function sum(num1, num2) {
+    var result = num1 + num2;
+    console.log(result);
+}
+sum(10, 89);
+var num = 0;
+while (num < 10) {
+    num++;
+    console.log(num);
+}
+var i = 0;
+for (i = 0; i <= 10; i++) {
+    console.log(i);
+}
+var fruits = "banana ";
+console.log(fruits.length);
+console.log(fruits.indexOf("nan"));
+console.log(fruits.slice(2, 6));
+console.log(fruits.replace("ban", 123));
+console.log(fruits.toUpperCase());
+console.log(fruits.toLocaleLowerCase());
+console.log(fruits.split(" "));
+console.log(fruits[2]);
+console.log(fruits.length);
+var fruits = ["banana", "orange", "apple"];
+console.log(fruits[2]);
+console.log(fruits);
+for (var i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+console.log(fruits.join("*"));
+console.log(fruits.pop(), fruits);
+
+console.log(fruits.push("mango"));
+console.log(fruits.join("*"));
+var vehicle = ["nissan", "toyota", "merced"];
+var sum = fruits.concat(vehicle);
+console.log(sum);
+console.log(sum.reverse());
+console.log(sum.sort()); 
+var age = prompt("what is your age");
+if (age => 20) {
+    console.log("you can vote");
+} else {
+    console.log("you cannot vote")
+}
+let marks = prompt("enter your marks");
+if ((marks => 0) && (marks <= 40)) {
+    console.log("fail");
+} else if ((marks => 41) && (marks <= 50)) {
+    console.log("good");
+} else if ((marks => 51) && (marks <= 70)) {
+    console.log("very good");
+} else if ((marks => 71) && (marks <= 100)) {
+    console.log("excellent");
+}
+var marks = prompt("enter your marks");
+switch (marks) {
+    case (marks => 0 && marks <= 39):
+        console.log(E);
+        break;
+    case (marks => 40 && marks <= 49):
+        console.log(D);
+        break;
+    case (marks => 50 && marks <= 59):
+        console.log(C);
+        break;
+    case (marks => 60 && marks <= 69):
+        console.log(E);
+        break;
+    case (marks => 70 && marks <= 100):
+        console.log(E);
+        break;
+    default:
+        console.log("invalid grade");
+
+}*/
